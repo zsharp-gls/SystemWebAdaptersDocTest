5ca147c2249cacc727dc45702e9993d02ef7af53
Zak Sharp <zsharp@greatlakessolutions.com>
2022-10-21 11:33:46 -0500

    Create sample class library

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ClassLibrary
+{
+    public class Class1
+    {
+        public bool PerformSomeCheck(HttpContext context)
+        {
+            return context.Request.Headers["SomeHeader"] == "ExpectedValue";
+        }
+    }
+}
