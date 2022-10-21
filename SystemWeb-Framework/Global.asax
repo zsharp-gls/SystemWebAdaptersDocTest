ff9211cd720625f2f5f4067f8ca43ed5a1609496
Zak Sharp <zsharp@greatlakessolutions.com>
2022-10-21 09:17:41 -0500

    Create .NET Framework ASP.NET app

---
+++ SystemWeb-Framework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SystemWeb_Framework.MvcApplication" Language="C#" %>
---
+++ SystemWeb-Framework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SystemWeb_Framework.Startup))]
+namespace SystemWeb_Framework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
