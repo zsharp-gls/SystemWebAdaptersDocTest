652118cb807c3086956a6d07048811dc50f12d33
Zak Sharp <zsharp@greatlakessolutions.com>
2022-10-21 11:39:05 -0500

    Use PerformSomeCheck in controller

---
+++ SystemWeb-Framework/Controllers/SomeController.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SystemWeb_Framework.Controllers
+{
+    public class SomeController : Controller
+    {
+        // GET: Some
+        public ActionResult Index()
+        {
+            var c1 = new Class1();
+            var checkResult = c1.PerformSomeCheck(System.Web.HttpContext.Current);
+            Console.WriteLine(checkResult);
+            return View();
+        }
+    }
+}
