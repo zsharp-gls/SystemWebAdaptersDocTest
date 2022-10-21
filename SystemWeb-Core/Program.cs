dd3e7860964e9fd563324dfb9fdcb8059789a87e
Zak Sharp <zsharp@greatlakessolutions.com>
2022-10-21 10:16:06 -0500

    following core.md

--- SystemWeb-Core/Program.cs
+++ SystemWeb-Core/Program.cs
@@ -1,6 +1,14 @@
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSystemWebAdapters();
+builder.Services.AddSystemWebAdapters()
+    //.AddJsonSessionSerializer(options => ClassLibrary.SessionUtils.RegisterSessionKeys(options)) //Commented out because ClassLibrary doesn't exist
+    .AddRemoteAppClient(options =>
+    {
+        options.RemoteAppUrl = new(builder.Configuration["ReverseProxy:Clusters:fallbackCluster:Destinations:fallbackApp:Address"]);
+        options.ApiKey = builder.Configuration["RemoteAppApiKey"];
+    })
+    .AddSessionClient();
+
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 // Add services to the container.
